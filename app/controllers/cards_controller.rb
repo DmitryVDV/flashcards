347a78faf9638683206a3fc7b07441da1c420627
Dmitry Vernikovsky <dmitry.vernikovsky@gmail.com>
2017-01-30 17:28:36 +0300

    Task - 3 - final

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,44 @@
 class CardsController < ApplicationController
-	def index
-		@cards = Card.all
-	end
+  def index
+    @cards = Card.all
+  end
+  
+  def show
+    @card = Card.find(params[:id])
+  end
+  
+  def new
+    @card = Card.new	
+  end
+
+  def edit
+  	@card =Card.find(params[:id])
+  end 
+
+  def create
+    @card = Card.new(card_params)
+    @card.save
+    redirect_to action: :index
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
+    	redirect_to @card
+    else
+    	render 'edit'
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to action: :index
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
+
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,28 @@
 class Card < ApplicationRecord
+  validates :translated_text, :original_text, presence: true
+  validates :translated_text, exclusion: {in: :original_text} 
+  validate :param_compare 
+  validate :space
+
+  before_create :plus_three_day
+  
+# Used regexp see https://docs.ruby-lang.org/en/trunk/Regexp.html
+  def param_compare
+  	if (original_text =~ /\A[[:lower:]]/ || translated_text =~ /\A[[:lower:]]/) && 
+  	   (original_text =~ /\A[[:upper:]]/ || translated_text =~ /\A[[:upper:]]/) 
+  		errors.add(:original_text, "The first letters must be eqal")
+  	end
+  end
+
+  def space
+    if (original_text =~ /\s/) || (translated_text =~ /\s/)
+      errors.add(:original_text, "There are space in string")
+  	end
+  end    
+
+  protected
+    def plus_three_day
+      self.review_date = 3.days.since
+    end
 end
+
