56c5a74c454480fef9a86bd8921b873b3442ffe4
Dmitry Vernikovsky <dmitry.vernikovsky@gmail.com>
2017-02-02 17:08:46 +0300

    Task 4 -

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  a = []
+  b = []
+  doc = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
+  links = doc.xpath("//table/tbody/tr")
+  
+  for i in 1..links.length-1
+    c = links.slice(i).children
+    b[0] = c.slice(3).content
+    b[1] = c.slice(5).content
+    a[i] = b
+    b = []
+  end
+
+  a.each do |s,c|
+    Card.create(original_text: s.to_s, translated_text: c.to_s)
+  end
