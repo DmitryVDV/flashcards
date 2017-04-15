c6dd0de7285b92e0bf40c098b9d7a3bada13dd68
Dmitry Vernikovsky <dmitry.vernikovsky@gmail.com>
2017-04-15 21:08:54 +0300

    try travise

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -5,22 +5,24 @@ describe 'Card - testing', :type => :feature do
 
     before(:each) do
         visit root_path
-        puts card.review_date
       end
-    let!(:card) { FactoryGirl.create(:card) }
+    #let!(:card) { FactoryGirl.create(:card) }
+    #let!(:card1) { FactoryGirl.create(:card) }
 
     it 'has main page' do
       expect(page).to have_content "Первый в мире удобный"
     end
 
     it 'has correct answer when input word for translation' do
+      card1 = FactoryGirl.create(:card)
       print page.html
-      fill_in "translation", with: card.translated_text
+      fill_in "translation", with: card1.translated_text
       click_button "Проверить"
       expect(page).to have_content("Перевод верный", wait:15)
     end
 
     it 'has uncorrect answer when input word for translation' do
+      FactoryGirl.create(:card)
       fill_in "translation", with: "Hl123"
       click_button "Проверить"
       expect(page).to have_content("Неверный ввод", wait:15)
