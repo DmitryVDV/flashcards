3b034632b900426a31abea56ef8c9cb15cdfcca4
Dmitry Vernikovsky <dmitry.vernikovsky@gmail.com>
2017-04-20 18:27:54 +0300

    Add rubocop.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    has_many :cards
+  has_many :cards
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Card, :type => :model do
 
-  it "plus three day to review_date" do
-    user_test = create(:user_with_card)
-    card_test = Card.last
-    expect(card_test.send(:plus_three_day).strftime("%Y %m %d")).to eq(3.days.since.strftime("%Y %m %d"))
-  end
-
   it "insert date field original_text without space in begin and all downcase" do
     user_test = create(:user_with_card)
     card_test = Card.last
