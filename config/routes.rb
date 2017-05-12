d8b3532e1619b003a5bb1edc4227b6194169e309
Dmitry Vernikovsky <dmitry.vernikovsky@gmail.com>
2017-05-12 10:07:59 +0300

    Devise - change user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'home#index'
   resources :cards
   post 'checktranslation' => 'home#meth_checktranslation'
--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,4 @@ ActiveRecord::Schema.define(version: 20170420101738) do
     t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
